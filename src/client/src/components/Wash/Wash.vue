9f557517f0d6650ff4ee250f9f00e4e21cb39449
rusultanbekov <rusultanbekov@ozon.travel>
2021-07-05 21:54:00 +0300

    feat: add Events News Sport and Wash component

---
+++ src/client/src/components/Wash/Wash.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Стирка</h1>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "Wash"
+})
+</script>
+
+<style scoped>
+
+</style>
