74dd802f8ad1772616fdbb0c382b727efc17890d
rusultanbekov <rusultanbekov@ozon.travel>
2021-07-05 21:40:46 +0300

    feat: move store and router in folder "src"

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from "@/client/router";
-import {store} from "@/client/store"
+import router from "@/client/src/router";
+import {store} from "@/client/src/store"
 
 createApp(App).use(router).use(store).mount('#app')
