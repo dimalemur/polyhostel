8659c7eaedae7b02f109f21668fbea3111cbe167
rusultanbekov <rusultanbekov@ozon.travel>
2021-07-05 21:39:37 +0300

    feat: add auth and mail layout

---
+++ src/client/src/layout/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar />
+  <router-view />
+</template>
+
+<script>
+import {defineComponent} from "vue"
+import Navbar from "../components/Navbar/Navbar";
+
+export default defineComponent({
+  name: "MainLayout",
+  components: {Navbar}
+})
+</script>
+
+<style scoped>
+
+</style>
