6f00f9cb860aa01be11025600a209a89089bb6b6
rusultanbekov <rusultanbekov@ozon.travel>
2021-07-05 21:41:00 +0300

    feat: add Navbar.vue

---
+++ src/client/src/components/Navbar/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav :class="$style.navbar">
+    <ul>
+      <li>
+        <router-link to="/news">Новости</router-link>
+      </li>
+      <li>
+        <router-link to="/events">Мероприятия</router-link>
+      </li>
+      <li>
+        <router-link to="/sport">Спорт</router-link>
+      </li>
+      <li>
+        <router-link to="/wash">Стирка</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import {defineComponent} from "vue"
+
+export default defineComponent({
+  name: "Navbar"
+})
+</script>
+
+<style lang="scss" module>
+  .navbar {
+
+  }
+</style>
