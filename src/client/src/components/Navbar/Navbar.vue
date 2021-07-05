529fdc32535ce17d9dca024f1080ff164e6e7ae9
rusultanbekov <rusultanbekov@ozon.travel>
2021-07-06 00:27:47 +0300

    feat: setup main layout and navbar

--- src/client/src/components/Navbar/Navbar.vue
+++ src/client/src/components/Navbar/Navbar.vue
@@ -2,18 +2,19 @@
   <nav :class="$style.navbar">
     <ul>
       <li>
-        <router-link to="/news">Новости</router-link>
+        <router-link to="/news" :class="[$style.link, $style.active]">Новости</router-link>
       </li>
       <li>
-        <router-link to="/events">Мероприятия</router-link>
+        <router-link to="/events" :class="$style.link">Мероприятия</router-link>
       </li>
       <li>
-        <router-link to="/sport">Спорт</router-link>
+        <router-link to="/sport" :class="$style.link">Спорт</router-link>
       </li>
       <li>
-        <router-link to="/wash">Стирка</router-link>
+        <router-link to="/wash" :class="$style.link">Стирка</router-link>
       </li>
     </ul>
+    <div :class="$style.separator"></div>
   </nav>
 </template>
 
@@ -27,6 +28,39 @@ export default defineComponent({
 
 <style lang="scss" module>
   .navbar {
-
+    width: 100%;
+    ul {
+      display: flex;
+      justify-content: center;
+    }
   }
+
+  .link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 300px;
+    height: 120px;
+    color: white;
+    text-decoration: none;
+    background-color: #874BA0;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  .separator {
+    width: 100%;
+    height: 11px;
+    background-color: #55B432;
+  }
+</style>
+
+<style lang="scss" scoped>
+
+  .active {
+    background-color: #55B432;
+  }
+
 </style>
--- src/client/src/layout/MainLayout.vue
+++ src/client/src/layout/MainLayout.vue
@@ -1,6 +1,20 @@
 <template>
-  <Navbar />
-  <router-view />
+  <div :class="$style.mainLayout">
+    <div :class="$style.firstNavigate">
+      <img src="@/client/assets/polylogoBlack.png" alt="Логотип политеха">
+      <button :class="$style.exitButton">Выход</button>
+    </div>
+    <div :class="$style.profileWrapper">
+      <div :class="$style.profile">
+        <p :class="$style.fio">Имя Фамилия</p>
+        <button :class="$style.settings">Настройки</button>
+      </div>
+      <div :class="$style.container">
+        <Navbar />
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,6 +27,65 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
+<style lang="scss" module>
+  .mainLayout {
+    height: 100%;
+    padding: 0 60px;
+    background-color: #E5E5E5;
+  }
 
+  .firstNavigate {
+    padding-top: 40px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .exitButton {
+    border-radius: 5px;
+    border: 3px solid black;
+    padding: 6px 18px;
+    font-size: 18px;
+    font-weight: 700;
+    cursor: pointer;
+    background-color: #E5E5E5;
+  }
+
+  .profileWrapper {
+    display: flex;
+    justify-content: center;
+    margin-top: 60px;
+    width: 100%;
+    position: relative;
+  }
+
+  .profile {
+    width: 100%;
+    padding: 36px 32px 110px;
+    background-color: black;
+    display: flex;
+    justify-content: space-between;
+
+    .fio {
+      color: white;
+      font-size: 35px;
+    }
+  }
+
+  .settings {
+    background-color: black;
+    color: white;
+    border: 3px solid white;
+    border-radius: 5px;
+    font-size: 30px;
+    font-weight: 400;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+
+  .container {
+    position: absolute;
+    top: 110px;
+    width: 1200px;
+  }
 </style>
