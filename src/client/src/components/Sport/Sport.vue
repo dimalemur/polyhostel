819b44a261bd866fa15eaca6182d12e00c58a843
Рустам <43078049+EzFroze@users.noreply.github.com>
2021-07-09 01:16:57 +0300

    feat: add sport and wash landing (#9)
    Co-authored-by: rusultanbekov <rusultanbekov@ozon.travel>

--- src/client/src/components/Sport/Sport.vue
+++ src/client/src/components/Sport/Sport.vue
@@ -1,15 +1,30 @@
 <template>
-  <h1>Спорт</h1>
+  <div :class="$style.container">
+    <DatePicker />
+    <div :class="$style.sportList">
+      <SportList v-for="key in 10" :key="key" />
+    </div>
+  </div>
 </template>
 
 <script>
 import {defineComponent} from "vue";
+import DatePicker from "@/client/src/components/Date/DatePicker";
+import SportList from "@/client/src/components/Sport/SportList";
 
 export default defineComponent({
-  name: "Sport"
+  name: "Sport",
+  components: {SportList, DatePicker}
 })
 </script>
 
-<style scoped>
+<style module lang="scss">
+  .container {
+    background: white;
+    padding: 12px 48px 32px;
+  }
 
+  .sportList {
+    padding-top: 20px;
+  }
 </style>
--- src/client/src/components/Wash/Wash.vue
+++ src/client/src/components/Wash/Wash.vue
@@ -1,15 +1,30 @@
 <template>
-  <h1>Стирка</h1>
+  <div :class="$style.container">
+    <DatePicker />
+    <div :class="$style.washList">
+      <WashList v-for="key in 3" :key="key" />
+    </div>
+  </div>
 </template>
 
 <script>
 import {defineComponent} from "vue";
+import DatePicker from "@/client/src/components/Date/DatePicker";
+import WashList from "@/client/src/components/Wash/WashList";
 
 export default defineComponent({
-  name: "Wash"
+  name: "Wash",
+  components: {WashList, DatePicker}
 })
 </script>
 
-<style scoped>
+<style module lang="scss">
+  .container {
+    padding: 12px 48px 32px;
+    background-color: white;
+  }
 
+  .washList {
+    padding-top: 20px;
+  }
 </style>
